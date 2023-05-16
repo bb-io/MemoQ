8d8fc4c12043f827399f7d9b09e6758858ff7299
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-05-16 15:54:11 +0300

    On document delivered dummy trigger

---
+++ Apps.Memoq/Callbacks/CallbacksList.cs
@@ -0,0 +1,30 @@
+﻿using Apps.Memoq.Callbacks.Payload;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace Apps.Memoq.Webhooks
+{
+    [WebhookList]
+    public class CallbacksList 
+    {
+        #region Deliver callbacks
+
+        [Webhook("On document delivered", Description = "On document delivered")]
+        public async Task<WebhookResponse<ProjectDeliveredDto>> ProjectCreation(WebhookRequest webhookRequest)
+        {
+            var data = JsonConvert.DeserializeObject<ProjectDeliveredDto>(webhookRequest.Body.ToString());
+            if(data is null)
+            {
+                throw new InvalidCastException(nameof(webhookRequest.Body));
+            }
+            return new WebhookResponse<ProjectDeliveredDto>
+            {
+                HttpResponseMessage = null,
+                Result = data
+            };
+        }
+
+        #endregion
+    }
+}
