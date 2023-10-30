686d2dc3dfc38c0a0684ce87e4d473c1267c07d8
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-10-30 15:06:26 +0200

    Artifactory

--- Apps.Memoq/Callbacks/Handlers/Base/WebhookHandler.cs
+++ Apps.Memoq/Callbacks/Handlers/Base/WebhookHandler.cs
@@ -1,15 +1,22 @@
 using Apps.Memoq.Extensions;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Webhooks;
 using Blackbird.Applications.Sdk.Utils.Webhooks.Bridge;
 using Blackbird.Applications.Sdk.Utils.Webhooks.Bridge.Models.Request;
 
 namespace Apps.Memoq.Callbacks.Handlers.Base;
 
-public abstract class WebhookHandler : BridgeWebhookHandler
+public class WebhookHandler : BaseInvocable, IWebhookEventHandler
 {
-    protected abstract string Event { get; }
+    public WebhookHandler(InvocationContext invocationContext) : base(invocationContext)
+    {
+    }
 
-    protected override (BridgeRequest webhookData, BridgeCredentials bridgeCreds) GetBridgeServiceInputs(
+    protected virtual string Event { get; }
+
+    protected (BridgeRequest webhookData, BridgeCredentials bridgeCreds) GetBridgeServiceInputs(
         Dictionary<string, string> values,
         IEnumerable<AuthenticationCredentialsProvider> creds)
     {
@@ -22,10 +29,22 @@ public abstract class WebhookHandler : BridgeWebhookHandler
 
         var bridgeCreds = new BridgeCredentials
         {
-            ServiceUrl = ApplicationConstants.BridgeServiceUrl,
+            ServiceUrl = $"{InvocationContext.UriInfo.BridgeServiceUrl.ToString().TrimEnd('/')}{ApplicationConstants.MemoqBridgePath}",
             Token = ApplicationConstants.BlackbirdToken
         };
 
         return (webhookData, bridgeCreds);
+    }
+
+    public async Task SubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> creds, Dictionary<string, string> values)
+    {
+        var (input, creds2) = GetBridgeServiceInputs(values, creds);
+        await BridgeService.Subscribe(input, creds2);
+    }
+
+    public async Task UnsubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> creds, Dictionary<string, string> values)
+    {
+        var (input, creds2) = GetBridgeServiceInputs(values, creds);
+        await BridgeService.Unsubscribe(input, creds2);
     }
 }
