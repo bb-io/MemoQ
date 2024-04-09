5fae92f7e510cdc3734eb11cc2965d258fc90efc
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-09 18:02:32 +0300

    Updated validator

--- Apps.Memoq/Connections/ConnectionValidator.cs
+++ Apps.Memoq/Connections/ConnectionValidator.cs
@@ -3,22 +3,27 @@ using Apps.Memoq.Models;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
 using MQS.Security;
-using RestSharp;
 
 namespace Apps.Memoq.Connections
 {
     public class ConnectionValidator : IConnectionValidator
     {
         public async ValueTask<ConnectionValidationResponse> ValidateConnection(
-       IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+            IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
         {
+            var apiKey = authProviders.FirstOrDefault(x => x.KeyName == "apiKey")?.Value;
+            if (apiKey != null && apiKey == "NONE")
+            {
+                return new() { IsValid = true };
+            }
+            
             try
             {
                 var securityService = new MemoqServiceFactory<ISecurityService>(SoapConstants.SecurityServiceUrl, authProviders);
 
                 var response = securityService.Service.ListUsers();
 
-                return new() { IsValid = response != null };
+                return new() { IsValid = response != null, Message = response == null ? "No response from server" : null };
             }
             catch (Exception ex)
             {
