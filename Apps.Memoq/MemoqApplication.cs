c5b78ea05b24610db4b20c19f3fba583c8bf9f80
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-11 12:54:06 +0300

    Implemented segments reimporting

--- Apps.Memoq/MemoqApplication.cs
+++ Apps.Memoq/MemoqApplication.cs
@@ -3,15 +3,8 @@ using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Memoq;
 
-public class MemoqApplication : IApplication, ICategoryProvider
+public class MemoqApplication : IApplication
 {
-    
-    public IEnumerable<ApplicationCategory> Categories
-    {
-        get => [ApplicationCategory.CatAndTms];
-        set { }
-    }
-    
     public string Name
     {
         get => "memoQ";
