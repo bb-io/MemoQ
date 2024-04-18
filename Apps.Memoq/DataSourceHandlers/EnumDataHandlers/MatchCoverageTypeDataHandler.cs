00edfee43634c5ab61c13a3289a4c9816442ced9
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-18 11:17:41 +0300

    Fixed pretranslation error

---
+++ Apps.Memoq/DataSourceHandlers/EnumDataHandlers/MatchCoverageTypeDataHandler.cs
@@ -0,0 +1,16 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+
+namespace Apps.Memoq.DataSourceHandlers.EnumDataHandlers;
+
+public class MatchCoverageTypeDataHandler : IStaticDataSourceHandler
+{
+    public Dictionary<string, string> GetData()
+    {
+        return new Dictionary<string, string>
+        {
+            {"100", "Full single hit"},
+            {"200", "Full multiple hits"},
+            {"300", "Not full"}
+        };
+    }
+}
--- Apps.Memoq/Models/Files/Requests/PretranslateDocumentsRequest.cs
+++ Apps.Memoq/Models/Files/Requests/PretranslateDocumentsRequest.cs
@@ -1,5 +1,6 @@
 ﻿using Apps.Memoq.DataSourceHandlers.EnumDataHandlers;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Memoq.Models.Files.Requests;
@@ -20,4 +21,31 @@ public class PretranslateDocumentsRequest
     
     [Display("Use MT")] 
     public bool? UseMt { get; set; }
+    
+    [Display("Translation memories GUIDs")]
+    public IEnumerable<string>? TranslationMemoriesGuids { get; set; }
+    
+    [Display("Include numbers", Description = "By default: true")]
+    public bool? IncludeNumbers { get; set; }
+    
+    [Display("Change case", Description = "By default: false")]
+    public bool? ChangeCase { get; set; }
+    
+    [Display("Include auto translations", Description = "By default: true")]
+    public bool? IncludeAutoTranslations { get; set; }
+    
+    [Display("Include fragments", Description = "By default: true")]
+    public bool? IncludeFragments { get; set; }
+    
+    [Display("Include non-translatables", Description = "By default: true")]
+    public bool? IncludeNonTranslatables { get; set; }
+    
+    [Display("Include term bases", Description = "By default: true")]
+    public bool? IncludeTermBases { get; set; }
+    
+    [Display("Minimum coverage", Description = "By default: 50")]
+    public int? MinCoverage { get; set; }
+    
+    [Display("Coverage type", Description = "By default: Not full"), StaticDataSource(typeof(MatchCoverageTypeDataHandler))]
+    public string? CoverageType { get; set; }
 }
