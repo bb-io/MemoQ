1bff0030951ccf23db26cb46a885cc53903c5407
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-08-06 13:40:25 +0200

    Added get reosurces assigned to project

---
+++ Apps.Memoq/Models/Files/Responses/ResourceListResponse.cs
@@ -0,0 +1,15 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.MemoQ.Models.Files.Responses
+{
+    public class ResourceListResponse
+    {
+        [Display("Resource IDs")]
+        public IEnumerable<string> ResourceIds { get; set; }
+    }
+}
