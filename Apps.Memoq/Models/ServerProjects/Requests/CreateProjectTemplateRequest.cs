4707cefdaab779bb565552e4db366ad3ac62b92c
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-04-07 15:02:37 +0300

    fix(requests): 🐛 update `ProjectAttribute` to `ProjectMetadata` in `CreateProjectTemplateRequest`; clarifies naming for better understanding

--- Apps.Memoq/Models/ServerProjects/Requests/CreateProjectTemplateRequest.cs
+++ Apps.Memoq/Models/ServerProjects/Requests/CreateProjectTemplateRequest.cs
@@ -34,6 +34,6 @@ public class CreateProjectTemplateRequest
     [Display("Subject")]
     public string? Subject { get; set; }
 
-    [Display("Project attribute", Description = "The project attribute of the project. Can be null and it will be taken into consideration if the template does not specify the project. It must be filled if the template refers to the project placeholder.")]
-    public string? ProjectAttribute { get; set; }
+    [Display("Project metadata", Description = "The project attribute of the project. Can be null and it will be taken into consideration if the template does not specify the project. It must be filled if the template refers to the project placeholder.")]
+    public string? ProjectMetadata { get; set; }
 }
