e3bac3a79593b0d242131433e86c703e03274908
RiabushenkoA <ariabushenko@blackbird.io>
2025-04-10 17:58:19 +0300

    Added checking for action 'Set custom field'

---
+++ Tests.MemoQ/ServerProjectTests.cs
@@ -0,0 +1,25 @@
+﻿using Apps.Memoq.Actions;
+using Apps.Memoq.Models.ServerProjects.Requests;
+using Apps.MemoQ.Models.ServerProjects.Requests;
+using Tests.MemoQ.Base;
+
+namespace Tests.MemoQ
+{
+    [TestClass]
+    public class ServerProjectTests : TestBase
+    {
+
+        [TestMethod]
+        public async Task Set_custom_values()
+        {
+            var handler = new ServerProjectActions(InvocationContext);
+
+            var value = "Hello";
+
+            await handler.SetCustomField(new ProjectRequest { ProjectGuid = "10bd767d-3ce2-ef11-875f-a8a15994f72e" },
+                new GetCustomFieldRequest { Field = "" }, value);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
