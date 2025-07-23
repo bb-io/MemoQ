0cb57ea0563be3a57c9724438d5ef0075e2c8dd4
RiabushenkoA <ariabushenko@blackbird.io>
2025-07-23 18:24:10 +0300

    Added event 'On task status changed' and action 'Start pretranslate files'

---
+++ Apps.Memoq/DataSourceHandlers/EnumDataHandlers/TaskStatusDataHandler.cs
@@ -0,0 +1,19 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+
+namespace Apps.MemoQ.DataSourceHandlers.EnumDataHandlers
+{
+    public class TaskStatusDataHandler : IStaticDataSourceHandler
+    {
+        Dictionary<string, string> IStaticDataSourceHandler.GetData()
+        {
+            return new Dictionary<string, string>
+            {
+                { "Pending", "Pending" },
+                { "Executing", "Executing" },
+                { "Completed", "Completed" },
+                { "Failed", "Failed" },
+                { "Cancelled", "Cancelled" }
+            };
+        }
+    }
+}
---
+++ Apps.Memoq/Events/Polling/Models/TaskStatusMemory.cs
@@ -0,0 +1,21 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.MemoQ.Events.Polling.Models
+{
+    public class TaskStatusMemory
+    {
+        public string Status { get; set; }
+        public DateTime LastCheckDate { get; set; }
+    }
+    public class TaskStatusResponse
+    {
+        [Display("Task ID")]
+        public string TaskId { get; set; }
+
+        [Display("Task status")]
+        public string TaskStatus { get; set; }
+
+        [Display("Progress percentage")]
+        public short ProgressPercentage { get; set; }
+    }
+}
