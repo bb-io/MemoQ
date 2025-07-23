3a9db4fc0c84e820b744f28f5f4167a4178d2f83
Alex Terehov <terales@users.noreply.github.com>
2025-07-23 07:04:15 -0230

    Introduce 'Prevent duplicate document upload' option to 'Upload file' action

--- Apps.Memoq/Models/Files/Requests/UploadDocumentToProjectWithOptionsRequest.cs
+++ Apps.Memoq/Models/Files/Requests/UploadDocumentToProjectWithOptionsRequest.cs
@@ -11,7 +11,7 @@ namespace Apps.MemoQ.Models.Files.Requests
     public class UploadDocumentToProjectWithOptionsRequest : ProjectRequest
     {
         [Display("File")]
-        public FileReference File { get; set; }
+        public FileReference File { get; set; } = new();
 
         [Display("Target languages"), StaticDataSource(typeof(TargetLanguageDataHandler))]
         public IEnumerable<string>? TargetLanguageCodes { get; set; }
@@ -32,5 +32,7 @@ namespace Apps.MemoQ.Models.Files.Requests
         [Display("Import embedded objects", Description = "Determines whether to import embedded objects from the file. Embedded objects are for example an embedded Excel spreadsheet in a Word document. Supported embedded objects will appear as translatable documents. Embedded objects can be imported from Office Open XML documents (docx, pptx, xlsx). If the specified filter configuration does not specify the configuration of embedded objects, they will be imported with default configurations.")]
         public bool? ImportEmbeddedObjects { get; set; }
 
+        [Display("Prevent duplicate document upload", Description = "Determines whether to prevent the import of duplicate documents (disabled by default). If this option is set, Blackbird won't upload document if one with the same name and for the same target language already exists in the project.")]
+        public bool? PreventDuplicateDocuments { get; set; }
     }
 }
