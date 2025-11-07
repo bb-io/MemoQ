67b12892cd1f21374d84982bae528e43e255339d
RiabushenkoA <ariabushenko@blackbird.io>
2025-11-07 20:28:56 +0200

    Error handler for project handler

--- Apps.Memoq/DataSourceHandlers/ProjectDataHandler.cs
+++ Apps.Memoq/DataSourceHandlers/ProjectDataHandler.cs
@@ -17,13 +17,21 @@ public class ProjectDataHandler : MemoqInvocable, IAsyncDataSourceItemHandler
 
     public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
     {
-        var projects = await ProjectService.Service.ListProjectsAsync(new ServerProjectListFilter());
+        try
+        {
+            var projects = await ProjectService.Service.ListProjectsAsync(new ServerProjectListFilter());
 
-        return projects
-            .Where(x => context.SearchString is null ||
-                        x.Name.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
-            .OrderByDescending(x => x.CreationTime)
-            .Take(20)
-            .Select(x => new DataSourceItem(x.ServerProjectGuid.ToString(), x.Name));
+            return projects
+                .Where(x => context.SearchString is null ||
+                            x.Name.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
+                .OrderByDescending(x => x.CreationTime)
+                .Take(20)
+                .Select(x => new DataSourceItem(x.ServerProjectGuid.ToString(), x.Name));
+        }
+        catch (Exception ex)
+        {
+            InvocationContext.Logger?.LogError($"[MemoQ error logger] Error fetching projects: {ex.Message} - {ex.InnerException} - {Newtonsoft.Json.JsonConvert.SerializeObject(ex)}", null);
+        }
+        return Array.Empty<DataSourceItem>();
     }
 }
