b53084eae371f0a3f67165aa6d5da2f30ee644f4
RiabushenkoA <ariabushenko@blackbird.io>
2025-11-07 20:32:47 +0200

    Error handler for project handler

--- Apps.Memoq/DataSourceHandlers/ProjectDataHandler.cs
+++ Apps.Memoq/DataSourceHandlers/ProjectDataHandler.cs
@@ -21,6 +21,8 @@ public class ProjectDataHandler : MemoqInvocable, IAsyncDataSourceItemHandler
         {
             var projects = await ProjectService.Service.ListProjectsAsync(new ServerProjectListFilter());
 
+            InvocationContext.Logger?.LogError($"[MemoQ error logger] Fetching projects: {Newtonsoft.Json.JsonConvert.SerializeObject(projects)}", null);
+
             return projects
                 .Where(x => context.SearchString is null ||
                             x.Name.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
